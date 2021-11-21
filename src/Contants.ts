b3ca66084cf77970ecb6386aa1f6653d8767b965
Emerson Silva <emerson_jdss@hotmail.com>
2021-11-21 08:23:00 -0300

    colors contants

---
+++ src/Contants.ts
@@ -0,0 +1,12 @@
+const Colors = {
+  black: '#000000',
+  white: '#ffffff',
+  gray: '#666666',
+  blue_green: '#52918b',
+  green: '#a7af5d',
+  medium_green: '#7a8440',
+  dark_green: '#2b3f3c',
+  orange: '#e48e1e',
+  dark_gray: '#434343',
+  light_blue: '#9dd1cd',
+}
